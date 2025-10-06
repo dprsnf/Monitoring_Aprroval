4a70df58fde2c29597c7d5d6cab112232b7c8ba9
giff <gifriend23@gmail.com>
2025-10-06 11:36:29 +0800

    feat: axios.ts for better api fetch and adding env example but ignore the real env using gitignore

---
+++ src/lib/axios.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const getCookieValue = (name: string): string | null => {
+  const cookies = document.cookie.split(";");
+  for (const cookie of cookies) {
+    const [cookieName, cookieValue] = cookie.split("=").map((c) => c.trim());
+    if (cookieName === name) {
+      return cookieValue;
+    }
+  }
+  return null;
+};
+
+const api = axios.create({
+  withCredentials: true,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Add a request interceptor to include the access token
+api.interceptors.request.use(
+  (config) => {
+    const token = getCookieValue("access_token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
